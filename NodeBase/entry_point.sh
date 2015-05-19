e48fcfc503a2c35c3577dddf6d192b50e602df4c
Petro Galushchak <petrogalushchak@gmail.com>
2015-05-19 11:41:21 +0300

    Adding container host ip parameter

--- NodeBase/entry_point.sh
+++ NodeBase/entry_point.sh
@@ -22,6 +22,7 @@ xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" \
   java -jar /opt/selenium/selenium-server-standalone.jar \
     -role node \
     -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
+    -host $CONTAINER_HOST \
     -nodeConfig /opt/selenium/config.json &
 NODE_PID=$!
 
